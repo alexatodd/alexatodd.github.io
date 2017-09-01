f774ac635bbe8a1539613593731cf46e7c295477
anorthfox <alexatodd@gmail.com>
2017-08-31 22:51:28 -0700

    reorganize files
    added folders to hold the application, style, and resources

--- src/app/navigation.js
+++ src/app/navigation.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
-	// Move logo out of navigation area on hover
-	var logo = $('#logo')
-	  , targets = $('#mainNavWrapper > div > ul > li:not(#logo)');
-	targets.unbind('mouseover');
-	targets.on('mouseover', function(){
-		var width = $(document).width()
-		  , top = '-120px';
-		logo.stop();
-		logo.animate({
-			top: top
-		}, 300);
-	});
-	targets.unbind('mouseout');
-	targets.on('mouseout', function(){
-		var width = $(document).width()
-		  , top = '-80px';
-		logo.stop();
-		logo.animate({
-			top: top
-		}, 300);
-	});
+$(document).ready(function() {
+  // Move logo out of navigation area on hover
+  var logo = $('#logo'),
+    targets = $('#mainNavWrapper > div > ul > li:not(#logo)');
+  targets.unbind('mouseover');
+  targets.on('mouseover', function() {
+    var width = $(document).width(),
+      top = '-120px';
+    logo.stop();
+    logo.animate({
+      top: top
+    }, 300);
+  });
+  targets.unbind('mouseout');
+  targets.on('mouseout', function() {
+    var width = $(document).width(),
+      top = '-80px';
+    logo.stop();
+    logo.animate({
+      top: top
+    }, 300);
+  });
 });
